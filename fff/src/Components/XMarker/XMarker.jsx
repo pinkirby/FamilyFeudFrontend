cef27828eef51b6ddc32a76abb8d054e673276f3
pinkirby <47227207+pinkirby@users.noreply.github.com>
2024-11-03 18:18:21 +0000

    Ajout de son pour XMarker

--- fff/src/Components/XMarker/XMarker.jsx
+++ fff/src/Components/XMarker/XMarker.jsx
@@ -1,3 +1,29 @@
+import 'flowbite';
+import { createSignal, onCleanup } from 'solid-js';
+import buzzerWrong from '../../assets/buzzerWrong.mp3';
+
 function XMarker() {
-    return <></>
+    const [marks, setMarks] = createSignal([]);
+    const audio = new Audio(buzzerWrong)
+    
+    const handleKeyPress = (e) => {
+        if (e.key === 'x' && marks().length < 3) {
+            setMarks([...marks(), <div class="text-red-500 text-9xl font-bold flex items-center justify-center">x</div>]);
+            audio.currentTime = 0;
+            audio.play();
+        }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+    onCleanup(() => window.removeEventListener('keydown', handleKeyPress));
+
+    return (
+        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="flex justify-around">
+                {marks()}
+            </div>
+        </div>
+    )
 }
+
+export default XMarker;
